d929f8e0cd84b92a9da652276af727f0483d4cd3
Wojciech Mocek <wojciech.mocek@o2.pl>
2023-10-27 02:41:30 +0200

    controllers

---
+++ app/Http/Controllers/PlaneController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePlaneRequest;
+use App\Http\Requests\UpdatePlaneRequest;
+use App\Models\Plane;
+
+class PlaneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json([
+            'data' => Plane::all(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePlaneRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $planeRegistration)
+    {
+        return response()->json([
+            'data' => Plane::where('registration', $planeRegistration)->firstOrFail(),
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Plane $plane)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePlaneRequest $request, Plane $plane)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Plane $plane)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PlaneReservationController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePlaneReservationRequest;
+use App\Http\Requests\UpdatePlaneReservationRequest;
+use App\Models\PlaneReservation;
+
+class PlaneReservationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePlaneReservationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PlaneReservation $planeReservation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PlaneReservation $planeReservation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePlaneReservationRequest $request, PlaneReservation $planeReservation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PlaneReservation $planeReservation)
+    {
+        //
+    }
+}
