e79429621d908dcd60c66f7d7322887e3650173e
Wojciech Mocek <wojciech.mocek@o2.pl>
2023-11-01 09:59:01 +0100

    fixed tests

--- tests/Feature/Services/PlaneReservationCheckerTest.php
+++ tests/Feature/Services/PlaneReservationCheckerTest.php
@@ -424,21 +424,74 @@ class PlaneReservationCheckerTest extends TestCase
 
         // assert
         $this->expectException(Exception::class);
-        $this->expectExceptionMessage('you can reserve planes for max 4 hours monthly');
+        $this->expectExceptionMessage('reservation overlaps with another reservation');
         
         // when
-        $this->service->checkUserMonthlyTimeLimit(
-            CarbonImmutable::parse('2021-01-10 13:00'),
-            CarbonImmutable::parse('2021-01-10 14:01'),
-            $user,
+        $this->service->checkReservationOverlaps(
+            CarbonImmutable::parse($start),
+            CarbonImmutable::parse($end),
+            '01HE1GB7NJSMF037F76BVR1D1M',
         );
     }
 
     public static function overlapingReservationsProvider(): iterable
     {
-        yield ['2021-01-10 13:00', '2021-01-10 14:01'];
-        yield ['2021-01-10 09:00', '2021-01-10 10:01'];
-        yield ['2021-01-10 12:30', '2021-01-10 17:00'];
-        yield ['2021-01-10 12:30', '2021-01-10 15:30'];
+        yield ['2021-01-01 10:00', '2021-01-01 14:01'];
+        yield ['2021-01-01 09:00', '2021-01-01 10:01'];
+        yield ['2021-01-15 12:30', '2021-01-16 17:00'];
+        yield ['2021-01-16 12:30', '2021-01-16 15:30'];
+    }
+
+    /**
+     * @test
+     * @dataProvider nonOverlapingReservationsProvider
+     */
+    public function whenReservationDoesNotOverlapItShouldBePossibleToReserve(string $start, string $end): void
+    {
+        // given
+        $user = new User([
+            'id' => Ulid::fromString('01HE1F50RYFHQS5HCTYWHDWYKY'),
+            'role' => UserRole::User
+        ]);
+
+        Plane::factory()->create(['id' => Ulid::fromString('01HE1GB7NJSMF037F76BVR1D1M')]);
+        Plane::factory()->create(['id' => Ulid::fromString('01HE1GRM0B8RQTEX4KYFT7Q7TR')]);
+        PlaneReservation::factory()->create([
+            'plane_id' => Ulid::fromString('01HE1GB7NJSMF037F76BVR1D1M'),
+            'user_id' => Ulid::fromString('01HE1F50RYFHQS5HCTYWHDWYKY'),
+            'starts_at' => '2021-01-01 10:00:00',
+            'ends_at' => '2021-01-01 10:59:00',
+            'time' => 60,
+        ]);
+        PlaneReservation::factory()->create([
+            'plane_id' => Ulid::fromString('01HE1GB7NJSMF037F76BVR1D1M'),
+            'user_id' => Ulid::fromString('01HE1F50RYFHQS5HCTYWHDWYKY'),
+            'starts_at' => '2021-01-16 15:00:00',
+            'ends_at' => '2021-01-16 16:00:00',
+            'time' => 60,
+        ]);
+        // this is different plane, it should not count
+        PlaneReservation::factory()->create([
+            'plane_id' => Ulid::fromString('01HE1GRM0B8RQTEX4KYFT7Q7TR'),
+            'user_id' => Ulid::fromString('01HE1F50RYFHQS5HCTYWHDWYKY'),
+            'starts_at' => '2021-01-10 13:00:00',
+            'ends_at' => '2021-01-10 14:00:00',
+            'time' => 60,
+        ]);
+        
+        // when
+        $this->service->checkReservationOverlaps(
+            CarbonImmutable::parse($start),
+            CarbonImmutable::parse($end),
+            '01HE1GB7NJSMF037F76BVR1D1M',
+        );
+        $this->assertTrue(true);
+    }
+
+    public static function nonOverlapingReservationsProvider(): iterable
+    {
+        yield ['2021-01-10 13:00', '2021-01-10 14:00'];
+        yield ['2021-01-10 09:00', '2021-01-10 09:59'];
+        yield ['2021-01-10 11:40', '2021-01-10 14:59'];
     }
 }
