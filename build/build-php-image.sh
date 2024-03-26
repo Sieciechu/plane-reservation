54f70df9b910d35684fb20b754ae08c0bf82e209
Wojciech Mocek <wojciech.mocek@o2.pl>
2024-03-26 09:18:55 +0100

    debug

--- build/build-php-image.sh
+++ build/build-php-image.sh
@@ -4,7 +4,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "Script dir: $SCRIPT_DIR"
 cd $SCRIPT_DIR
-docker image build -f Dockerfile  --build-arg WWWUSER=501 --build-arg WWWGROUP=501 -t planereservation:php8.3 .
-docker image build -f Dockerfile  --build-arg WWWUSER=501 --build-arg WWWGROUP=501 -t planereservation:dev --target dev .
+docker image build -f Dockerfile --build-arg WWWUSER=501 --build-arg WWWGROUP=501 -t planereservation:php8.3 .
+docker image build -f Dockerfile --build-arg WWWUSER=501 --build-arg WWWGROUP=501 -t planereservation:dev --target dev .
 
 cd -
